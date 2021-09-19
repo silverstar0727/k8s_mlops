00e6071749ae6808a32e755ea5bd8905251269e1
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-09-19 13:56:11 +0900

    Update kubeflow.sh

--- kubeflow/kubeflow.sh
+++ kubeflow/kubeflow.sh
@@ -1,7 +1,7 @@
 # 설치 폴더 이름 정하기
 export KF_NAME=my-kubeflow
 # 기본 디렉토리 설정
-export BASE_DIR=kubeflowsetup
+export BASE_DIR=~/Desktop
 # 설치 디렉토리 설정
 export KF_DIR=${BASE_DIR}/${KF_NAME}
 # 폴더 생성후 이동
@@ -21,3 +21,6 @@ kfctl apply -V -f ${CONFIG_URI}
 
 # 모두 running 될때까지 기다리기
 kubectl get all -n kubeflow
+
+# port-forwarding으로 8080포트로 띄우기
+kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80
