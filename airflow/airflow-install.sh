94405c384950ea1014eef41c9fec098bbb1dd9ff
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-10-06 00:35:50 +0900

    airflow loadbalancer

--- airflow/airflow-install.sh
+++ airflow/airflow-install.sh
@@ -1,3 +1,4 @@
 kubectl create namespace airflow
 helm repo add apache-airflow https://airflow.apache.org
 helm install airflow apache-airflow/airflow --namespace airflow
+kubectl expose deployment airflow-webserver --type=LoadBalancer --name=web-lb -n airflow
