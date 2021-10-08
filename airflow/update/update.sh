0940f52d1dae3fdc33c9056cef999ac207f13f4b
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-10-09 00:15:34 +0900

    airflow update

--- airflow/update/update.sh
+++ airflow/update/update.sh
@@ -4,3 +4,12 @@ helm show values apache-airflow/airflow > values.yaml
 helm upgrade --install airflow apache-airflow/airflow -n airflow -f values.yaml --debug 
 # load docker image
 kind load docker-image silverstar456/k8s_mlops:airflow-custom
+
+
+# second sol
+helm upgrade -n airflow \
+  --install airflow apache-airflow/airflow \
+  --set images.airflow.repository=silverstar456/k8s_mlops \
+  --set images.airflow.tag=airflow-custom \
+  --set images.airflow.pullPolicy=Always \
+  --debug
