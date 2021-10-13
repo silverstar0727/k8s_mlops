b8129b85a1a0026b30655375e31c54d5d83942e6
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-10-14 02:16:24 +0900

    rename and delete cluster command

---
+++ gke.sh
@@ -0,0 +1,14 @@
+# us-central1-a 를 기본 리전으로 선택합니다.
+gcloud config set compute/zone us-central1-a
+
+# k8s를 클러스터 이름으로 지정합니다.
+CLUSTER_NAME=k8s-ml
+
+# 클러스터를 만들때 
+gcloud container clusters create $CLUSTER_NAME \
+    --num-nodes=4 \
+    --node-labels=role=default \
+    --machine-type=n1-standard-2 \
+    --node-locations=us-central1-a
+
+gcloud container clusters delete $CLUSTER_NAME
